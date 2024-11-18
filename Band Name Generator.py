ddc61075adc4aa30e2ded218e9953731d25709b2
Nabil003 <ibrahimkhalilnabil321@gmail.com>
2024-11-18 17:29:35 +0000

    Initial commit

---
+++ Band Name Generator.py
@@ -0,0 +1,5 @@
+# Band name generator
+print("Welcome to Band Name Generator")
+city = input("Which city did you grow up in?\n")
+petname = input("What is your petname?\n")
+print("Your band name could be " + city + " " + petname)
